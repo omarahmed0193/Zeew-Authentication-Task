84cbdd74303b4f7463deb568541b48dcd5127f44
Omar Ahmed <omarahmed019395@gmail.com>
2021-09-03 23:24:18 +0200

    - Added error handling for network errors

--- app/src/main/java/com/zeew/ui/signup/SignupViewModel.kt
+++ app/src/main/java/com/zeew/ui/signup/SignupViewModel.kt
@@ -10,6 +10,7 @@ import com.zeew.repository.AuthenticationRepository
 import com.zeew.util.RegexValidationUtil
 import com.zeew.util.Resource
 import dagger.hilt.android.lifecycle.HiltViewModel
+import io.reactivex.rxjava3.kotlin.subscribeBy
 import javax.inject.Inject
 
 @HiltViewModel
